00ff4005af72573dad0afd86803a39a8982aafa4
JesiahChow <S10221066@connect.np.edu.sg>
2024-01-21 03:33:26 +0800

    added database connection, images, css and some pages

---
+++ header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mamaya e-BookStore</title>
+    <!--site specific cascading stylesheet-->
+    <link rel="stylesheet" href="css/site.css">
+</head>
+
+<body>
+    <div class="container">
+        <!--1st row-->
+        <div class="row">
+            <div class="col-sm-12">
+                <a href="index.php">
+                    <img src="Images/mamayaebooks.jpg" alt="Logo" class="img-fluid" style="width:100%" /></a>
+            </div>
+        </div>
+        <!--2nd row -->
+        <div class="row">
+            <div class="col-sm-12">
+                <?php include("navbar.php"); ?>
+            </div>
+        </div>
+        <!--3rd row -->
+        <div class="row">
+            <div class="col-sm-12" style="padding:15px;">
+                <!--define customised content here-->
+                <!-- closing of this column and row is in footer.php-->
+
+</body>
+
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,67 @@
+<?php
+
+//Display guest welcome message, Login and Registration links
+//when shopper has yet to login,
+$content1 = "Welcome Guest<br />";
+$content2 = "<li class='nav-item'>
+		     <a class='nav-link' href='register.php'>Sign Up</a></li>
+			 <li class='nav-item'>
+		     <a class='nav-link' href='login.php'>Login</a></li>";
+if (isset($_SESSION["ShopperName"])) {
+
+
+    //Display a greeting message, Change Password and logout links 
+    //after shopper has logged in.
+    $content1 = "Welcome <b>$_SESSION[ShopperName]</b>";
+    $content2 = "<li class='nav-item'>
+                <a class='nav-link'href='changePassword.php'>Change Password</a></li>
+                <li class='nav-item'>
+                <a class='nav-link' href='logout.php'>Logout</a></li>";
+
+
+    //Display number of item in cart
+    if(isset($_SESSION["NumCartItem"])){
+        $content1 .= ", $_SESSION[NumCartItem] item(s) in shopping cart";
+    }
+}
+?>
+
+     Display a navbar which is visible before or after collapsing -->
+<nav class="navbar navbar-expand-md navbar-dark bg-custom">
+    <div class="container-fluid">
+        <!--Dynamic text display-->
+        <span class="navbar-text ms-md-2" style="color:#F7BE81; max-width: 80%;">
+            <?php echo $content1; ?>
+        </span>
+        <!--Toggler/Collapsible button-->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+
+<!--
+     Define a collapsible navbar -->
+<nav class="navbar navbar-expand-md navbar-dark bg-custom">
+    <div class="container-fluid">
+        <!--collapsible part of navbar-->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <!--left-justified menu items-->
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="category.php">Product Categories</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="search.php">Product Search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="shoppingCart.php">Shopping Cart</a>
+                </li>
+            </ul>
+            <!--right-justified menu items-->
+            <ul class="navbar-nav ms-auto">
+                <?php echo $content2; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
