ff7425efc9f43a9ac96e5b3bf9f3fe9ffd502e38
OngYiOngYi <you@example.com>
2024-02-01 11:16:51 +0800

    ／＞　 フ                | 　_　_|           ／` ミ＿xノ          /　　　　 |         /　 ヽ　　 ﾉ             へ     ╱|、 ♡  ╱|、         │　　|　|　|       ૮  -   ՛ )  (˚ˎ 。7    (`   -  7 ／￣|　　 |　|　|         /   ⁻  ៸|   |、˜〵     |、⁻〵 (￣ヽ＿_ヽ_)__)   乀 (ˍ, ل ل  じしˍ,)ノ じしˍ,)ノ ＼二)

---
+++ shipInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+// Add PayPal Checkout button on the shopping cart page
+echo "<form method='post' action=' checkoutProcess.php'>";
+echo "<p style='text-align:right; font-size:20px'>";
+echo "Delivery Method: <input type='text' name='shipMethod' value='Normal Delivery'><br />";
+echo "</p>";
+echo "<input type='image' style='float:right;'
+						src='https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif'>";
+echo "</form>";
+?>	
