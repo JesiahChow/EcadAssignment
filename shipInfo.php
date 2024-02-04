f99bf96f124016d281eab7164d14f054a6074167
SeahXiangYu <seahxiangyu1234@gmail.com>
2024-02-04 18:10:53 +0800

    Delivery Fee waived for over $200

--- shipInfo.php
+++ shipInfo.php
@@ -8,8 +8,13 @@ $options = array("Normal Delivery" => 5, "Express Delivery" => 10);
 
 echo "Delivery Method: <select name='shipMethod'>";
 
+$subTotal = $_SESSION["SubTotal"];
+
 foreach ($options as $option => $cost) {
     $selected = ($option == 'Normal Delivery') ? 'selected' : ''; // Set default value as 'Normal Delivery'
+    if ($subTotal >= 200) {
+        $selected = ($option == 'Express Delivery') ? 'selected' : '';
+    }
     echo "<option value='$option' $selected>$option ($$cost)</option>";
 }
 
@@ -18,4 +23,3 @@ echo "</p>";
 echo "<input type='image' style='float:right;'
 						src='https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif'>";
 echo "</form>";
-?>	
